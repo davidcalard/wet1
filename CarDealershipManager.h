3329f6989a1cda2ebb97f1824f57a32cddf470d2
davidcalard <58255839+davidcalard@users.noreply.github.com>
2021-05-10 18:49:45 +0300

    Update CarDealershipManager.h

--- CarDealershipManager.h
+++ CarDealershipManager.h
@@ -10,31 +10,34 @@
 #include "AVLTree.h"
 #include "ModelGrades.h"
 
+
 typedef int CarID,index,Sales_num;
 class Car{
+public:
     Sales_num* sales;			//num of sales of each model
     int models_num;		//num of models
-    std::pair<index,Sales_num> best_m_seller;
-public:
+    std::pair<index,Sales_num> best_m_seller = std::make_pair(0,0);
     Car(int n):models_num(n){
         sales = new int[n];
+        for(int i=0;i<n;i++)    sales[i]=0;
+        best_m_seller;
     }
     ~Car(){delete[] sales;}
+
+    bool updateSales(int model){
+        if(model>=models_num)return false;
+        else sales[model]++;
+        return true;
+    }
 };
 
 
 
 
 class CarDealershipManager {
-    AVLTree<CarID,Car> cars;
+public:
     AVLTree<int,AVLTree<ModelGrades,ModelGrades>> models_grades;
-    std::pair<int,int> best_c_seller;
+    std::pair<CarID ,Sales_num> best_c_seller = std::make_pair(0,0);
+    AVLTree<CarID,Car> cars;
 
 };
-
-
-
-#endif //WET1_CARDEALERSHIPMANAGER_H
-
-
-
