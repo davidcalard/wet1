e6a8b60cc96c6e43d9ac9881f9eebbdd92262599
omergino <82971041+omergino@users.noreply.github.com>
2021-05-10 17:20:36 +0300

    Update CarDealershipManager.cpp

--- CarDealershipManager.cpp
+++ CarDealershipManager.cpp
@@ -9,9 +9,15 @@ void* Init(){
     return (void*)(new CarDealershipManager());
 }
 
+StatusType AddCarType(void *DS, int typeID, int numOfModels){
+    if(!DS||typeID<=0||numOfModels<=0) return FAILURE;         //bad argument
+    CarDealershipManager *ourDS = static_cast<CarDealershipManager*>(DS);
+    Car new_car(numOfModels);
+    ourDS->cars.insertNode(typeID,new_car);
+}
 
-
-StatusType GetBestSellerModelByType(void *DS, int typeID, int * modelID){
+/*
+ * StatusType GetBestSellerModelByType(void *DS, int typeID, int * modelID){
     if(DS== nullptr || typeID<0)return INVALID_INPUT;
     auto* CDM=(CarDealershipManager*)DS;
     if(typeID==0){
@@ -29,8 +35,8 @@ StatusType GetWorstModels(void *DS, int numOfModels , int *types, int *models){
     ///AVLNode<>* father = CDM.WorstModel;
     ///do
     ///father.models.preOrder()   /*inner tree*/
-
-    ///if(father.right.preOrder()) /*bigger tree*/
-    ///father= father.father;
-    ///while(father!= nullptr)
-}
+/*
+///if(father.right.preOrder()) /*bigger tree*/
+///father= father.father;
+///while(father!= nullptr)
+///}
