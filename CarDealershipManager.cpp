70fd34aca7c5c3a3f00856554eccf5c0755e08bc
davidcalard <58255839+davidcalard@users.noreply.github.com>
2021-05-10 18:48:30 +0300

    Update CarDealershipManager.cpp

--- CarDealershipManager.cpp
+++ CarDealershipManager.cpp
@@ -46,6 +46,25 @@ StatusType MakeComplaint(void *DS, int typeID, int modelID, int t){
     return SUCCESS;
 }
 
+StatusType SellCar(void *DS, int typeID, int modelID){
+    if(!DS||typeID<=0||modelID<0)   return INVALID_INPUT;
+    auto* ourDS = static_cast<CarDealershipManager*>(DS);
+    AVLNode<CarID,Car> *car_to_sell = ourDS->cars.findNode(typeID);
+    if(!car_to_sell) return FAILURE;
+    if(!car_to_sell->data.updateSales(modelID))    return INVALID_INPUT;//modelID > numOfModels
+    if(car_to_sell->data.best_m_seller.second == car_to_sell->data.sales[modelID])// update best_m_seller if needed
+        if(modelID < car_to_sell->data.best_m_seller.first) {
+            car_to_sell->data.best_m_seller.first = modelID;
+            car_to_sell->data.best_m_seller.second = car_to_sell->data.sales[modelID];
+        }
+    if(ourDS->best_c_seller.second == car_to_sell->data.sales[modelID])
+        if(typeID < ourDS->best_c_seller.first){
+            ourDS->best_c_seller.first = typeID;
+            ourDS->best_c_seller.second = car_to_sell->data.sales[modelID];
+        }
+    return SUCCESS;
+}
+
 /*
  * StatusType GetBestSellerModelByType(void *DS, int typeID, int * modelID){
     if(DS== nullptr || typeID<0)return INVALID_INPUT;
