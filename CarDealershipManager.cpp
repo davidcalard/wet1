1cbc6657b7209965a7aa419244e8493dec3e64a4
omergino <82971041+omergino@users.noreply.github.com>
2021-05-10 20:29:37 +0300

    Add files via upload

--- CarDealershipManager.cpp
+++ CarDealershipManager.cpp
@@ -34,14 +34,14 @@ StatusType MakeComplaint(void *DS, int typeID, int modelID, int t){
     int oldGrade=car->sales[modelID];
     car->sales[modelID]-= 100/t;
     int NewGrade=car->sales[modelID];
-    CDM->models_grades.findNode(oldGrade)->data.removeNode(oldGrade);
+    CDM->models_grades.findNode(oldGrade)->data.removeNode(ModelGrades(typeID,oldGrade));
     AVLNode<int,AVLTree<ModelGrades,ModelGrades>>* grade=CDM->models_grades.findNode(NewGrade);
     if(grade!= nullptr) grade->data.insertNode
-    (ModelGrades(typeID,modelID),ModelGrades(typeID,modelID));
+                (ModelGrades(typeID,modelID),ModelGrades(typeID,modelID));
     else {
         CDM->models_grades.insertNode(NewGrade,AVLTree<ModelGrades,ModelGrades>());
         CDM->models_grades.findNode(NewGrade)->data.insertNode
-        (ModelGrades(typeID,modelID),ModelGrades(typeID,modelID));
+                (ModelGrades(typeID,modelID),ModelGrades(typeID,modelID));
     }
     return SUCCESS;
 }
@@ -65,27 +65,34 @@ StatusType SellCar(void *DS, int typeID, int modelID){
     return SUCCESS;
 }
 
-/*
- * StatusType GetBestSellerModelByType(void *DS, int typeID, int * modelID){
+StatusType GetBestSellerModelByType(void *DS, int typeID, int * modelID){
+    /*
     if(DS== nullptr || typeID<0)return INVALID_INPUT;
     auto* CDM=(CarDealershipManager*)DS;
     if(typeID==0){
         *modelID= CDM->getBestSellerTypeId();
         return SUCCESS;
     }
+     */
+    return SUCCESS;
     ///car = CDM.CArsTree.find(key=typeID);
     ///if(car==null)return fail
     ///*modelID=car.best;
     ///SUCCESS
 }
+
 StatusType GetWorstModels(void *DS, int numOfModels , int *types, int *models){
-    if(DS== nullptr || numOfModels<=0)return INVALID_INPUT;
+    /*if(DS== nullptr || numOfModels<=0)return INVALID_INPUT;
     auto* CDM=(CarDealershipManager*)DS;
-    ///AVLNode<>* father = CDM.WorstModel;
-    ///do
-    ///father.models.preOrder()   /*inner tree*/
-/*
+    auto father = CDM->models_grades.min;
+    do
+    father->data.min   /*inner tree*/
+    return SUCCESS;
+
 ///if(father.right.preOrder()) /*bigger tree*/
 ///father= father.father;
 ///while(father!= nullptr)
-///}
+
+}
+
+void Quit(void **DS){}
